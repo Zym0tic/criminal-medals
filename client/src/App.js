fb77043bcff7d5c699980e6348c0bf55ab11ba80
zym0tic <lukerozon@gmail.com>
2022-10-06 16:50:47 -0400

    add jumbotron to home

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Nav from './components/nav/index.js'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/home.js";
+import Nav from "./components/nav/index.js";
 
 function App() {
   return (
     <Router>
       <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
-};
+}
 
 export default App;
---
+++ client/src/pages/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Jumbotron from "../components/jumbotron";
+
+const Home = () => {
+    return (
+        <Jumbotron />
+    );
+};
+
+export default Home;
