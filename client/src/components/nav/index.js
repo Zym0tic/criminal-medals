526a0bee39d1b9d14606319d20d747bb3200d89f
zym0tic <lukerozon@gmail.com>
2022-10-06 17:36:31 -0400

    add functiuonality to dropdown/change class to className

--- client/src/components/nav/index.js
+++ client/src/components/nav/index.js
@@ -1,50 +1,50 @@
 import React from "react";
 import cmLogo from "../../assets/images/cm-logo.jpg";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Nav() {
     return (
     <>
-     <nav class="navbar navbar-expand-lg navbar-light">
+     <nav className="navbar navbar-expand-lg navbar-light">
 
-<a class="navbar-brand" href="index.html">
-  <img class="cm-logo" src= {cmLogo} alt=""/>
+<a className="navbar-brand" href="/">
+  <img className="cm-logo" src= {cmLogo} alt=""/>
 </a>
 
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02">
-  <span class="navbar-toggler-icon"></span>
+<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02">
+  <span className="navbar-toggler-icon"></span>
 </button>
 
-<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+<div className="collapse navbar-collapse" id="navbarTogglerDemo02">
 
 
-  <ul class="navbar-nav ml-auto">
-    <li class="nav-item">
-      <a class="nav-link" href="index.html">Home</a>
+  <ul className="navbar-nav ml-auto">
+    <li className="nav-item">
+      <a className="nav-link" href="/">Home</a>
     </li>
 
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Collections</a>
-      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-        <a class="dropdown-item" href="bracelets.html">Bracelets</a>
-        <a class="dropdown-item" href="cord-necklaces.html">Cord Necklaces</a>
-        <a class="dropdown-item" href="sterling.html">Sterling Silver</a>
-        <a class="dropdown-item" href="rings.html">Rings</a>
-        <a class="dropdown-item" href="chains.html">Chain Necklaces</a>
-        <a class="dropdown-item" href="earring.html">Earrings</a>
+    <li className="nav-item dropdown">
+      <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Collections</a>
+      <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+        <a className="dropdown-item" href="/bracelets">Bracelets</a>
+        <a className="dropdown-item" href="cord-necklaces.html">Cord Necklaces</a>
+        <a className="dropdown-item" href="sterling.html">Sterling Silver</a>
+        <a className="dropdown-item" href="rings.html">Rings</a>
+        <a className="dropdown-item" href="chains.html">Chain Necklaces</a>
+        <a className="dropdown-item" href="earring.html">Earrings</a>
       </div>
     </li>
 
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">faq</a>
-      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-        <a class="dropdown-item" href="custom-orders.html">Custom Orders</a>
-        <a class="dropdown-item" href="shipping-refund.html">Shipping//Refund Policy</a>
+    <li className="nav-item dropdown">
+      <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">faq</a>
+      <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+        <a className="dropdown-item" href="custom-orders.html">Custom Orders</a>
+        <a className="dropdown-item" href="shipping-refund.html">Shipping//Refund Policy</a>
       </div>
     </li>
 
-    <li class="nav-item">
-      <a class="nav-link" href="about.html">About</a>
+    <li className="nav-item">
+      <a className="nav-link" href="about.html">About</a>
     </li>
   </ul>
 
