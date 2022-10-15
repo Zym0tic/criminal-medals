bcf131a95057ee984cd018a9b7ca44ecc8e84477
Sean Dillon <pdillon0@gmail.com>
2022-10-14 20:45:38 -0400

    fixed the react server

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,32 @@ import Home from "./pages/home.js";
 import Signup from "./pages/signup.js";
 import Nav from "./components/nav/index.js";
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <Nav />
       <Routes>
@@ -21,6 +45,7 @@ function App() {
         {/* <Route path="/signup" element={<Signup />} /> */}
       </Routes>
     </Router>
+    </ApolloProvider>
   );
 }
 
