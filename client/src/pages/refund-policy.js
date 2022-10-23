958a11dabe71bc8f9007672c917aee241d7a5dca
zym0tic <lukerozon@gmail.com>
2022-10-23 15:01:41 -0400

    add content to return and care instructions

---
+++ client/src/pages/refund-policy.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Return = () => {
+  return (
+    <>
+      <h2>Return Policy</h2>
+      <p>
+      Criminal medals does not accept returns unless it is found there are flaws in the workmanship. The item must be in its unworn and original condition. We reserve the right to deny a refund to sender without prior return approval
+      </p>
+    </>
+  );
+};
+
+export default Return;
