e4290d707941be58c6a04e44daf144eb9ece24a5
zym0tic <lukerozon@gmail.com>
2022-11-15 16:57:25 -0500

    add category menu component and edit shop page

---
+++ client/src/pages/shop.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductList from "../components/ProductList";
+
+const Shop = () => {
+  return (
+    <div className="container">
+      <ProductList />
+    </div>
+  );
+};
+
+export default Shop;
