262aa2bb66c49455e17fe4825ad2c86f6071286a
zym0tic <lukerozon@gmail.com>
2022-11-15 13:31:48 -0500

    add bootstrap classes to signup

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -5,38 +5,38 @@ import Auth from "../utils/auth";
 import { ADD_USER } from "../utils/mutations";
 
 const Signup = () => {
-  const [formState, setFormState] = useState({ 
-    email: "", 
-    firstName: "", 
+  const [formState, setFormState] = useState({
+    email: "",
+    firstName: "",
     lastName: "",
-    password: "" }
-    );
+    password: "",
+  });
 
   const [addUser, { error, data }] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
-    // event.preventDefault();
-    // const mutationResponse = await addUser({
-    //   variables: {
-    //     email: formState.email,
-    //     password: formState.password,
-    //     firstName: formState.firstName,
-    //     lastName: formState.lastName,
-    //   },
-    // });
-    // const token = mutationResponse.data.addUser.token;
-    // Auth.login(token);
+    event.preventDefault();
+    const mutationResponse = await addUser({
+      variables: {
+        email: formState.email,
+        password: formState.password,
+        firstName: formState.firstName,
+        lastName: formState.lastName,
+      },
+    });
+    const token = mutationResponse.data.addUser.token;
+    Auth.login(token);
     event.preventDefault();
     console.log(formState);
 
     try {
-        const { data } = await addUser({
-            variables: { ...formState },
-        });
+      const { data } = await addUser({
+        variables: { ...formState },
+      });
 
-        Auth.login(data.addUser.token);
+      Auth.login(data.addUser.token);
     } catch (e) {
-        console.error(e);
+      console.error(e);
     }
   };
 
@@ -50,56 +50,65 @@ const Signup = () => {
 
   return (
     <div className="container my-1">
-    <Link to="/login">← Go to Login</Link>
+      <Link to="/login">← Go to Login</Link>
 
-    <h2>Signup</h2>
-    <form onSubmit={handleFormSubmit}>
-      <div className="flex-row space-between my-2">
-        <label htmlFor="firstName">First Name:</label>
-        <input
-          placeholder="First"
-          name="firstName"
-          type="firstName"
-          id="firstName"
-          onChange={handleChange}
-        />
-      </div>
-      <div className="flex-row space-between my-2">
-        <label htmlFor="lastName">Last Name:</label>
-        <input
-          placeholder="Last"
-          name="lastName"
-          type="lastName"
-          id="lastName"
-          onChange={handleChange}
-        />
-      </div>
-      <div className="flex-row space-between my-2">
-        <label htmlFor="email">Email:</label>
-        <input
-          placeholder="youremail@test.com"
-          name="email"
-          type="email"
-          id="email"
-          onChange={handleChange}
-        />
-      </div>
-      <div className="flex-row space-between my-2">
-        <label htmlFor="pwd">Password:</label>
-        <input
-          placeholder="******"
-          name="password"
-          type="password"
-          id="pwd"
-          onChange={handleChange}
-        />
-      </div>
-      <div className="flex-row flex-end">
-        <button type="submit">Submit</button>
-      </div>
-    </form>
-  </div>
+      <h2>Signup</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="row">
+          <div className="form-group col-md-12 col-lg-6">
+            <label htmlFor="firstName">First Name:</label>
+            <input
+              className="form-control"
+              placeholder="First"
+              name="firstName"
+              type="firstName"
+              id="firstName"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group col-md-12 col-lg-6">
+            <label htmlFor="lastName">Last Name:</label>
+            <input
+              className="form-control"
+              placeholder="Last"
+              name="lastName"
+              type="lastName"
+              id="lastName"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="form-group col-md-12 col-lg-6">
+            <label htmlFor="email">Email:</label>
+            <input
+              className="form-control"
+              placeholder="youremail@test.com"
+              name="email"
+              type="email"
+              id="email"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group col-md-12 col-lg-6">
+            <label htmlFor="pwd">Password:</label>
+            <input
+              className="form-control"
+              placeholder="******"
+              name="password"
+              type="password"
+              id="pwd"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="flex-row flex-start">
+          <button className="btn btn-outline-dark btn-lg insta-button" type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
   );
-}
+};
 
 export default Signup;
